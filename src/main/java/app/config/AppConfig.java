eba96c07310b8ac88d584c3125748b9923d45060
Vladimir Dymov <77827028+VovaDym@users.noreply.github.com>
2025-02-09 13:37:01 +0300

    solution

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,18 @@
 package app.config;
 
+import app.model.AnimalsCage;
+import app.model.Dog;
+import app.model.Timer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
+    @Bean
+    public Timer getTimer() {
+        Timer timer = new Timer();
+        return timer;
+    }
 }
